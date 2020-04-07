0685fb06a285eeaea061bc7465db7be028f2dece
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-04-07 11:25:29 +0200

    Implement all methods related to medical record information

--- src/main/java/com/safetynet/alerts/dao/MedicalRecordDAOImpl.java
+++ src/main/java/com/safetynet/alerts/dao/MedicalRecordDAOImpl.java
@@ -1,11 +1,128 @@
-/*
 package com.safetynet.alerts.dao;
 
+import com.safetynet.alerts.AlertsObjectMapper;
+import com.safetynet.alerts.model.MedicalRecord;
+import com.safetynet.alerts.model.Objects;
+import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class MedicalRecordsDAOImpl implements MedicalRecordsDAO {
-    méthodes génériques
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+@Component
+public class MedicalRecordDAOImpl implements MedicalRecordDAO {
+
+    @Autowired
+    private Utils utils;
+
+    Objects objects = AlertsObjectMapper.readDataObjects();
+    List<MedicalRecord> medicalRecords = objects.getMedicalrecords();
+    List<Person> persons = objects.getPersons();
+
+    @Override
+    public MedicalRecord findMinor() throws ParseException {
+        MedicalRecord medicalRecordMinor = new MedicalRecord();
+
+        for (MedicalRecord medicalRecord : medicalRecords) {
+            boolean isMinor = utils.resolveMinorPeopleFromDate(medicalRecord.getBirthdate());
+            if (isMinor) {
+                medicalRecordMinor.setFirstName(medicalRecord.getFirstName());
+                medicalRecordMinor.setLastName(medicalRecord.getLastName());
+                medicalRecordMinor.setBirthdate(medicalRecord.getBirthdate());
+                return medicalRecordMinor;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int countAdultFromList(List<Person> coveredPersons) throws ParseException {
+        int adultCount = 0;
+
+        for (Person person : coveredPersons) {
+            for (MedicalRecord medicalRecord : medicalRecords) {
+                if (medicalRecord.getFirstName().contentEquals(person.getFirstName()) && medicalRecord.getLastName().contentEquals(person.getLastName())) {
+                    boolean isMinor = utils.resolveMinorPeopleFromDate(medicalRecord.getBirthdate());
+                    if (!isMinor) {
+                        adultCount++;
+                    }
+                }
+            }
+        }
+        return adultCount;
+    }
+
+    @Override
+    public int countChildFromList(List<Person> coveredPersons) throws ParseException {
+        int childCount = 0;
+
+            for (Person person : coveredPersons) {
+                for (MedicalRecord medicalRecord : medicalRecords) {
+                    if (medicalRecord.getFirstName().contentEquals(person.getFirstName()) && medicalRecord.getLastName().contentEquals(person.getLastName())) {
+                        boolean isMinor = utils.resolveMinorPeopleFromDate(medicalRecord.getBirthdate());
+                        if (isMinor) {
+                             childCount++;
+                        }
+                    }
+                }
+            }
+            return childCount;
+        }
+
+    @Override
+    public List<MedicalRecord> getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    @Override
+    public List<MedicalRecord> getMedicalRecordsFromAddress(String address) {
+        List<MedicalRecord> medicalRecordList = new ArrayList<>();
+
+        for (Person person : persons) {
+            if (person.getAddress().contentEquals(address)) {
+                for (MedicalRecord medicalRecord : medicalRecords) {
+                    if (medicalRecord.getFirstName().contentEquals(person.getFirstName()) && medicalRecord.getLastName().contentEquals(person.getLastName())) {
+                        medicalRecordList.add(medicalRecord);
+                    }
+                }
+            }
+        }
+        return medicalRecordList;
+    }
+
+
+
+    @Override
+    public void addMedicalRecord(MedicalRecord medicalRecord) {
+        medicalRecords.add(medicalRecord);
+    }
+
+    @Override
+    public String removeMedicalRecord(String firstName, String lastName) {
+        for (MedicalRecord medicalRecord : medicalRecords) {
+            if (medicalRecord.getFirstName().contentEquals(firstName) && medicalRecord.getLastName().contentEquals(lastName)) {
+                medicalRecords.remove(medicalRecord);
+                return "Medical record deleted: " + medicalRecord;
+            }
+        }
+        return "Medical record not found";
+    }
+
+    @Override
+    public String modifyMedicalRecord(MedicalRecord medicalRecord) {
+        int pos = 0;
+
+        for (MedicalRecord md : medicalRecords) {
+            if (md.getFirstName().contentEquals(medicalRecord.getFirstName()) && md.getLastName().contentEquals(medicalRecord.getLastName())) {
+                medicalRecords.set(pos, medicalRecord);
+            }
+            return "Modified medical record : " + medicalRecord.getFirstName() + " " + medicalRecord.getLastName();
+        }
+        return "Medical record not found";
+    }
 }
-*/
+
