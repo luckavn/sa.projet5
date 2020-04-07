042fe4951bdfe72ba850deb687387bf972d5bd5d
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-04-07 11:20:28 +0200

    Configure Spring boot actuator endpoints to show httptrace, health, info and metrics

--- src/main/java/com/safetynet/alerts/configuration/Config.java
+++ src/main/java/com/safetynet/alerts/configuration/Config.java
@@ -1,4 +1,16 @@
 package com.safetynet.alerts.configuration;
 
-public class Configuration {
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository()
+    {
+        return new InMemoryHttpTraceRepository();
+    }
 }
--- src/main/java/com/safetynet/alerts/configuration/HealthIndicator.java
+++ src/main/java/com/safetynet/alerts/configuration/HealthIndicator.java
@@ -2,18 +2,14 @@ package com.safetynet.alerts.configuration;
 
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class HealthIndicator extends AbstractHealthIndicator {
 
     @Override
     protected void doHealthCheck(Health.Builder builder) throws Exception {
-        // Use the builder to build the health status details that should be reported.
-        // If you throw an exception, the status will be DOWN with the exception message.
-
         builder.up()
-                .withDetail("app", "Alive and Kicking")
-                .withDetail("error", "Nothing! I'm good.");
+                .withDetail("app", "Alive and Kicking");
     }
 }
